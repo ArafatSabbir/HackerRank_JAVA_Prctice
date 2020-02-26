11bee677ddc26043285cc16f23a6d5570625cb04
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-02-26 23:59:08 +0600

    Variables name and code update.

--- Java Subarray.java
+++ Java Subarray.java
@@ -3,21 +3,21 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] array1= new int[n];
+        int arraySize=sc.nextInt();
+        int[] arr= new int[arraySize];
         
-        for(int i=0;i<n;i++){
-        array1[i]=sc.nextInt();
+        for(int i=0;i<arraySize;i++){
+        arr[i]=sc.nextInt();
         }
 
 
         int count=0;
-        for(int j=0;j<n;j++){
+        for(int j=0;j<arraySize;j++){
             int sum=0;
-            for(int k=j;k<n;k++){
-                sum=array1[k]+sum;
+            for(int k=j;k<arraySize;k++){
+                sum=arr[k]+sum;
                 if(sum<0){
                 count++;
             }
